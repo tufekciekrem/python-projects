2b7b9af6e9d852065d24258fcb18da6ba80a5682
Ekrem Tüfekçi <ekremtufekci@gmail.com>
2023-01-31 15:47:18 +0300

    proje1 yazildi.

--- project1/main.py
+++ project1/main.py
@@ -1,7 +1,7 @@
 x = int(input("Bir sayı giriniz"))
 while x < 1:
     if x < 0:
-        print("Sayı negatif, lütfen pozitif bir sayı giriniz")
+        print("Sayı negatif, lütfen pozitif bir sayı giriniz.")
     if x==0:
         print("sayı sıfırdır pozitif gir")
     x = int(input("Bir sayı giriniz:"))
